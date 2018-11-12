01000dc7d4b5e22181ea4aa4cd45f1b940277853
rwinklewski <rene.winklewski@gmail.com>
2018-11-12 12:16:08 -0800

    extend session

--- src/Session.php
+++ src/Session.php
@@ -4,6 +4,10 @@ namespace Microshard\Application;
 
 class Session
 {
+    const MESSAGE_NOTICE = 'notice';
+    const MESSAGE_SUCCESS = 'success';
+    const MESSAGE_ERROR = 'error';
+
     /**
      * @return Session
      */
@@ -37,9 +41,44 @@ class Session
      * @param string $key
      * @return Session
      */
-    public function uns(string $key): self
+    public function remove(string $key): self
     {
         unset($_SESSION[$key]);
+        return $this;
+    }
+
+    /**
+     * @param string $message
+     * @param null|string $type
+     * @return Session
+     */
+    public function addMessage(string $message, ?string $type = self::MESSAGE_NOTICE): self
+    {
+        $messages = $this->get('frontend_messages');
+        if (!$messages) {
+            $messages = [];
+        }
+        $messages[] = ['type' => $type, 'message' => $message];
+
+        $this->set('frontend_messages', $messages);
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getMessages(): array
+    {
+        $messages = $this->get('frontend_messages');
+        return $messages ?? [];
+    }
+
+    /**
+     * @return Session
+     */
+    public function clearMessages(): self
+    {
+        $this->set('frontend_messages', []);
         return $this;
     }
 }
